e73803a694703a152c4f24d30d45cbaf379a0a19
AVue2 <Vuey00@gmail.com>
2025-04-17 16:52:00 -0400

    Added images folder to finalize README. Comments updated as well

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,4 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// Interface for the Candidate objects returned by the API
 interface Candidate {
     name: string;
     username: string;
